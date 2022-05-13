6ee42f6c4be683760255abcc1f0ccd50558a6f8d
Daniel Cruz <danielcunha54@gmail.com>
2022-05-13 18:19:12 -0300

    Add products to cart

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,31 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 export const CartContext = createContext({
   isCartOpen: false,
-  setIsCartOpen: () => {}
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  cartCount: 0
 })
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false)
-  const value = { isCartOpen, setIsCartOpen }
+  const [cartItems, setCartItems] = useState([])
+  const [cartCount, setCartCount] = useState(0)
 
+  useEffect(() => {
+    const newCartCount = cartItems.reduce((count, item) => count + item.quantity, 0)
+    setCartCount(newCartCount)
+  }, [cartItems])
+
+  const addItemToCart = (product) => {
+    const itemsMap = cartItems.reduce((map, item) => ({ ...map, [item.id]: item }), {})
+    itemsMap[product.id] = itemsMap[product.id] || { ...product, quantity: 0 }
+    itemsMap[product.id].quantity++
+
+    setCartItems(Object.values(itemsMap))
+  }
+
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount }
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
